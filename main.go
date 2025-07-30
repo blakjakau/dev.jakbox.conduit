ef7462639c6278354b4e6e2fb0ffa42add5dbb1e
Jason <blakjak.au@gmail.com>
2025-07-30 11:43:24 +0800

    enable detection of compliled status

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"flag"
 	"log"
-	"os"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
 // --- Configuration ---
-const version = "0.1.0"
+const version = "0.1.1"
 const port = "3022"
 
 var allowedOrigins = map[string]bool{
@@ -22,10 +24,30 @@ var rootFlag string // New flag for file API root
 var keyFlag bool // New flag for key management
 var debugLogging bool
 var requiredAPIKey string // Stores the API key if --key is used. Empty if no key is required.
+var isCompiledBuild bool // flag to indicate running from compiled binary
+var fileAPIRoot string  // fileAPIRoot is set by main based on CLI args or defaults to user's home dir.
 
-// fileAPIRoot is set by main based on CLI args or defaults to user's home dir.
-var fileAPIRoot string 
+
+func getIsCompiled() {
+	// Determine if running as a compiled build based on the executable name.
+	exePath, err := os.Executable()
+	
+	// log.Printf("exePath %s", exePath)
+	if err != nil {
+		log.Printf("Warning: Could not determine executable path: %v", err)
+	} else {
+		exeName := filepath.Base(exePath)
+		// log.Printf("exeName %s", exeName)
+		// Compiled builds are expected to be named "conduit-[os]-[arch]"
+		
+		isCompiledBuild = strings.HasPrefix(exeName, "conduit-") && (len(strings.Split(exeName, "-"))>2)
+	}
+}
+
 func main() {
+	
+	getIsCompiled()
+	
 	flag.BoolVar(&debugLogging, "debug", false, "Enable debug logging")
 	flag.BoolVar(&keyFlag, "key", false, "Manage and print the API key for no-origin requests, then exit.")
 	flag.Parse()
@@ -53,6 +75,7 @@ func main() {
 	go fileWatcher.run()
 	startTime = time.Now()
 	mux := http.NewServeMux()
+	log.Printf("Running as compiled build: %t", isCompiledBuild)
 	mux.HandleFunc("/terminal", terminalServer)
 	mux.HandleFunc("/up", upcheckHandler)
 	mux.HandleFunc("/files", filesApiHandler)
